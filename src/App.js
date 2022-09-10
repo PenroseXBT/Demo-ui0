bc406e9e392bfb8978814cdfc2990ce12422e1fb
jacksturtevant <jack.a.sturtevant@gmail.com>
2022-09-10 17:17:53 -0600

    add connect wallet button

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Dashboard2 } from "./Dashboard2";
-
-export default function App() {
-  document.body.style.backgroundColor = "#1e1e1e";
-  document.body.style.margin = "0";
-  return <Dashboard2 />;
-}
